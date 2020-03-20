016bcd39a357a8c25b59f9a5b25e8c67b06b595b
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-03-20 14:08:01 +0700

    Initial commit

---
+++ assets/js/Chart.js
@@ -0,0 +1,49 @@
+(function ($) {
+ "use strict";
+
+/*----------------------------
+    Chart bundle.js
+------------------------------ */
+var ctx = document.getElementById("bissChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug" ],
+        datasets: [{
+            label: '# of Profit',
+            data: [3, 10, 8, 15, 17, 14, 22 , 26],
+            backgroundColor: [
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)',
+                'rgba(248,179,42, 0.2)'
+            ],
+            borderColor: [
+                'rgba(248,179,42, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});    
+    
+    
+    
+})(jQuery); 
