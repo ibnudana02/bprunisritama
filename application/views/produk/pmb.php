21c6183026104122ad2adbf4d1aca102b35af7ef
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-03-27 17:15:41 +0700

    PMB added

---
+++ application/views/produk/pmb.php
@@ -0,0 +1,169 @@
+<!--Blog Area Start-->
+<div class="blog-page-area area-padding">
+    <div class="container">
+        <div class="row">
+            <div class="blog-details">
+                <!-- Start single blog -->
+                <div class="col-md-8 col-sm-8 col-xs-12">
+                    <!-- single-blog start -->
+                    <article class="blog-post-wrapper">
+                        <div class="blog-banner">
+                            <a href="#" class="blog-images">
+                                <img src="<?= base_url('assets/') ?>img/blog/pmb.jpg" alt="">
+                            </a>
+                            <div class="blog-content">
+                                <div class="blog-category">
+                                    <span>Pembayaran Mahasiswa</span>
+                                </div>
+                                <h4>The universal acceptance of Consultation has given a tremendous</h4>
+
+                                <p>The universal acceptance of Consultation has given a tremendous opportunity for merchants to do crossborder transactions instantly and at reduced cost.Consultations are slowly gaining immense recognition and are growing phenomenally with more and more people trading with this digital currency.</p>
+                                <blockquote>
+                                    <p>Consultations are slowly gaining immense recognition and are growing phenomenally with more and more people trading with this digital currency. The universal acceptance of Consultation has given a tremendous opportunity for merchants to do crossborder transactions instantly and at reduced cost.</p>
+                                </blockquote>
+                                <p>The universal acceptance of Consultation has given a tremendous opportunity for merchants to do crossborder transactions instantly and at reduced cost.Consultations are slowly gaining immense recognition and are growing phenomenally with more and more people trading with this digital currency. The universal acceptance of Consultation has given a tremendous opportunity for merchants to do crossborder transactions instantly and at reduced cost.</p>
+                            </div>
+                        </div>
+                    </article>
+                    <div class="clear"></div>
+                </div>
+                <!-- single-blog end -->
+            </div>
+            <!-- End main column -->
+            <!-- End main column -->
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <div class="left-head-blog">
+                    <div class="left-blog-page">
+                        <!-- search option start -->
+                        <form action="#">
+                            <div class="blog-search-option">
+                                <input type="text" placeholder="Search...">
+                                <button class="button" type="submit">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </div>
+                        </form>
+                        <!-- search option end -->
+                    </div>
+                    <div class="left-blog-page">
+                        <!-- recent start -->
+                        <div class="left-blog">
+                            <h4>recent post</h4>
+                            <div class="recent-post">
+                                <!-- start single post -->
+                                <div class="recent-single-post">
+                                    <div class="post-img">
+                                        <a href="#">
+                                            <img src="<?= base_url('assets/') ?>img/blog/b1.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="pst-content">
+                                        <p><a href="#">Designer have to make sure the prototype looks.</a>
+                                            <div class="blog-meta">
+                                                <span class="date-type">
+                                                    <i class="fa fa-calendar"></i>
+                                                    10 feb, 2018
+                                                </span>
+                                            </div>
+                                    </div>
+                                </div>
+                                <!-- End single post -->
+                                <!-- start single post -->
+                                <div class="recent-single-post">
+                                    <div class="post-img">
+                                        <a href="#">
+                                            <img src="<?= base_url('assets/') ?>img/blog/b2.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="pst-content">
+                                        <p><a href="#">The phrasal sequence of the Lorem Ipsum text.</a></p>
+                                        <div class="blog-meta">
+                                            <span class="date-type">
+                                                <i class="fa fa-calendar"></i>
+                                                13 july, 2017
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End single post -->
+                                <!-- start single post -->
+                                <div class="recent-single-post">
+                                    <div class="post-img">
+                                        <a href="#">
+                                            <img src="<?= base_url('assets/') ?>img/blog/b3.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="pst-content">
+                                        <p><a href="#">Make sure the prototype looks finished by.</a></p>
+                                        <div class="blog-meta">
+                                            <span class="date-type">
+                                                <i class="fa fa-calendar"></i>
+                                                25 may, 2017
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End single post -->
+                                <!-- start single post -->
+                                <div class="recent-single-post">
+                                    <div class="post-img">
+                                        <a href="#">
+                                            <img src="<?= base_url('assets/') ?>img/blog/b4.jpg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="pst-content">
+                                        <p><a href="#">The phrasal sequence of the Lorem Ipsum text.</a></p>
+                                        <div class="blog-meta">
+                                            <span class="date-type">
+                                                <i class="fa fa-calendar"></i>
+                                                20 jan, 2018
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- End single post -->
+                            </div>
+                        </div>
+                        <!-- recent end -->
+                    </div>
+                    <div class="left-blog-page">
+                        <div class="left-blog">
+                            <h4>categories</h4>
+                            <ul>
+                                <li><a href="#">Business</a></li>
+                                <li><a href="#">Industry </a></li>
+                                <li><a href="#">Tax Planning</a></li>
+                                <li><a href="#">Online Consulting</a></li>
+                                <li><a href="#">Maketing Expert</a></li>
+                                <li><a href="#">Consulting</a></li>
+                                <li><a href="#">Business Planning</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="left-blog-page">
+                        <div class="left-tags blog-tags">
+                            <div class="popular-tag left-side-tags left-blog">
+                                <h4>popular tags</h4>
+                                <ul>
+                                    <li><a href="#">Business</a></li>
+                                    <li><a href="#">Industry </a></li>
+                                    <li><a href="#">Tax</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li><a href="#">Online</a></li>
+                                    <li><a href="#">Consulting</a></li>
+                                    <li><a href="#">Maketing</a></li>
+                                    <li><a href="#">Expert</a></li>
+                                    <li><a href="#">Consulting</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End left sidebar -->
+        </div>
+        <!-- End row -->
+    </div>
+</div>
+</div>
+<!--End of Blog Area-->
