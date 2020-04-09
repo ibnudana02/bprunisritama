5d3249e0858ff28e900647a7035d9f25ead67c12
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-04-09 16:48:12 +0700

    category

---
+++ application/controllers/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Controllername extends CI_Controller
+{
+
+    public function index()
+    {
+    }
+}
+
+/* End of file Controllername.php */
