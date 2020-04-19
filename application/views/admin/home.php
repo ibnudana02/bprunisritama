06d8fa91bfa791c4653da35f1322cbaa05d9e03f
ibndana <muhammadibnuwardana@gmail.com>
2020-04-19 11:23:42 +0700

    Signed-off-by: ibndana <muhammadibnuwardana@gmail.com>

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -24,6 +24,7 @@
                 </div>
             </div>
         </div>
+        <!-- start card for produk -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
@@ -41,6 +42,7 @@
                 </div>
             </div>
         </div>
+        <!-- start card for berita -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-info shadow h-100 py-2">
                 <div class="card-body">
@@ -58,6 +60,7 @@
                 </div>
             </div>
         </div>
+        <!-- start card for publikasi  -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
