d05d29073418c7d8ddc3b6506d60e8c1f69f459b
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-04-20 14:49:28 +0700

    DB

---
+++ application/controllers/admin/Publikasi.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Publikasi extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+    }
+
+
+    public function index()
+    {
+    }
+}
+
+/* End of file Publikasi.php */
