140853051e94c94a000e08c6955a007454c74f5e
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-04-29 16:22:03 +0700

    cap

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -4,10 +4,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 // To use reCAPTCHA, you need to sign up for an API key pair for your site.
 // link: http://www.google.com/recaptcha/admin
 
-$config['recaptcha_site_key'] = '6LfW2e4UAAAAABXxoqUqW_DWlExJVnucIB7CeZ7c';
-// $config['recaptcha_site_key'] = '6LeBzO8UAAAAAC6dvxpg7N1aRgq6eH5SkHfS7pyi';
-$config['recaptcha_secret_key'] = '6LfW2e4UAAAAAFLC-sHUuS-edcZ6bx246g1jN6hS';
-// $config['recaptcha_secret_key'] = '6LeBzO8UAAAAAJZ-GsHsd-mD_HEoA3ak8-slFL4s';
+// $config['recaptcha_site_key'] = '6LfW2e4UAAAAABXxoqUqW_DWlExJVnucIB7CeZ7c';
+$config['recaptcha_site_key'] = '6LeBzO8UAAAAAC6dvxpg7N1aRgq6eH5SkHfS7pyi';
+// $config['recaptcha_secret_key'] = '6LfW2e4UAAAAAFLC-sHUuS-edcZ6bx246g1jN6hS';
+$config['recaptcha_secret_key'] = '6LeBzO8UAAAAAJZ-GsHsd-mD_HEoA3ak8-slFL4s';
 // reCAPTCHA supported 40+ languages listed here:
 // https://developers.google.com/recaptcha/docs/language
 $config['recaptcha_lang'] = 'en';
