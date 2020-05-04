d2d3ac6ea95e9b85b56c43972917462dbbf7b9f1
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-05-04 11:03:21 +0700

    Profil

--- application/views/tentang/profil.php
+++ application/views/tentang/profil.php
@@ -20,17 +20,26 @@
             <div class="col-md-6 col-sm-6 col-xs-12">
                 <div class="about-content">
                     <h4>PT. BPR Unisritama</h4>
-                    <p class="hidden-sm text-justify">Dibentuk pada tanggal 5 Februari 1991 dengan memakai nama PT. Bank Perkreditan Rakyat Unisri. Tak lama setelah terbentuk PT. BPR Unisri kembali merubah nama perseroan menjadi PT. Bank Perkreditan Rakyat Unisritama pada tanggal 1 April 1991 yang kemudian dipakai hingga saat ini. Bank Unisritama memiliki makna yang berarti "Bank Universitas Islam Riau Pertama" berdasarkan waktu pendiriannya.</p>
+                    <p class="hidden-sm text-justify">Dibentuk pada tanggal 6 Februari 1991 dengan memakai nama PT. Bank Perkreditan Rakyat Unisri. Tak lama setelah terbentuk PT. BPR Unisri kembali merubah nama perseroan menjadi PT. Bank Perkreditan Rakyat Unisritama pada tanggal 1 April 1991 yang kemudian dipakai hingga saat ini. Bank Unisritama memiliki makna yang berarti "Bank Universitas Islam Riau Pertama" berdasarkan waktu pendiriannya.</p>
                     <p class="hideen-sm text-justify">Pemegang saham Perseroan adalah Yayasan Lembaga Pendidikan Islam Riau (YLPI Riau) dengan jumlah saham 85,62%, bersama dengan Bapak H. Imam Ghazali, SH, MH dengan jumlah saham 14,38% yang juga merupakan salah satu putra dari pendiri PT. BPR Unisritama.</p>
-                    <div class="about-details">
-                        <div class="single-about">
-                            <a href="#"><i class="icon icon-chart-bars"></i></a>
-                            <div class="icon-text">
-                                <h5>Certified company</h5>
-                                <p>The phrasal sequence of the Lorem Ipsum text is now so widespread that many the starting sequence</p>
-                            </div>
-                        </div>
-                    </div>
+                    <h5 class="classic-title">
+                        <span>Identitas Perseroan</span>
+                    </h5>
+                    <p><b>Nama Perusahaan :</b>
+                        <br> PT. Bank Perkreditan Rakyat Unisritama
+                    </p>
+                    <p><b>NPWP :</b>
+                        <br>01.496.657.6-221.000
+                    </p>
+                    <p><b>Legalitas :</b>
+                        <br>Akta Notaris
+                    </p>
+                    <p><b>Tanggal Pembentukan :</b>
+                        <br>6 Februari 1991
+                    </p>
+                    <p><b>Alamat :</b>
+                        <br>Jalan Soekarno-Hatta No. 03, Kubang Jaya, Kabupaten Kampar
+                    </p>
                 </div>
             </div>
         </div>
