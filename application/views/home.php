ab51c73655ba8e67d857e8b34cbf22b4484de25d
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-05-05 12:21:38 +0700

    Still style slider berita

--- application/views/home.php
+++ application/views/home.php
@@ -11,7 +11,6 @@
             <div class="intro-berita">
                 <?php foreach ($berita as $row) : ?>
                     <div class="slider-berita">
-                        <!-- <div class="col-md-4 col-sm-4 col-xs-12"> -->
                         <div class="well-services">
                             <div class="services-img">
                                 <a href="" class="">
@@ -27,32 +26,10 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- </div> -->
                     </div>
                 <?php endforeach; ?>
             </div>
         </div>
-        <!-- <div class="row">
-            <?php foreach ($berita as $row) : ?>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <div class="well-services">
-                        <div class="services-img">
-                            <a href="" class="image-scale">
-                                <img src="<?= base_url('upload/berita/' . $row->image) ?>" alt="">
-                            </a>
-                            <span class="top-icon"><i class="flaticon-graph-6"></i></span>
-                        </div>
-                        <div class="main-services">
-                            <div class="service-content">
-                                <h4><?= $row->kategori; ?></h4>
-                                <p><?= strtoupper($row->judul); ?></p>
-                                <a class="service-btn" href="#">read more</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
-        </div> -->
     </div>
 </div>
 <div class="services-area">
