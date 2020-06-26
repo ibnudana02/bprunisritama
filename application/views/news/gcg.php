39e1edf024e21853617420c8b06ad9cca4d4d00d
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-06-26 17:45:01 +0700

    20200626-04

--- application/views/news/gcg.php
+++ application/views/news/gcg.php
@@ -9,7 +9,7 @@
                             <div class="blog-content">
                                 <div class="blog-title" style="padding: 0 25px;">
                                     <a>
-                                        <h5 class="text-center"><?= $row->laporan ?></h5>
+                                        <h5 class="text-center"><?= ucfirst($row->laporan) ?></h5>
                                     </a>
                                 </div>
                                 <div class="single-page-head">
@@ -30,8 +30,8 @@
                                     <h5 class="modal-title mt-0 text-center"><?= strtoupper($row->laporan); ?></h5>
                                 </div>
                                 <div class="modal-body" style="height: 420px;width:fit-content;">
-                                    <embed src="<?= base_url('upload/laporan/' . $row->dokumen) ?>" type='application/pdf' width='100%' height='420px' />
-                                    <!-- <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=" .<?= base_url('upload/laporan/' . $row->dokumen) ?> frameborder="0" width="100%" height="100%"></iframe> -->
+                                    <!-- <embed src="<//?= base_url('upload/laporan/' . $row->dokumen) ?>" type='application/pdf' width='100%' height='420px' /> -->
+                                    <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=https://web.bprunisritama.com/upload/laporan/<?= $row->dokumen ?>" frameborder="0"></iframe>
                                 </div>
                                 <div class="modal-footer" style="margin-top: 25px;">
                                     <button type="button" class="btn btn-danger center-block" data-dismiss="modal" data-toggle="tooltip" data-placement="top" title="Tutup">Tutup</button>
