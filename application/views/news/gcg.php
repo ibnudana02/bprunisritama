3da96337c04edc096e3ceecde87408340e546624
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-06-29 16:02:43 +0700

    20200629-02

--- application/views/news/gcg.php
+++ application/views/news/gcg.php
@@ -31,7 +31,7 @@
                                 </div>
                                 <div class="modal-body" style="height: 420px;width:100%;">
                                     <!-- <embed src="<//?= base_url('upload/laporan/' . $row->dokumen) ?>" type='application/pdf' width='100%' height='420px' /> -->
-                                    <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=<?= base_url('upload/laporan' . $row->dokumen) ?>" width='100%' height='420px' frameborder="0"></iframe>
+                                    <iframe id="pdfviewer" src="http://docs.google.com/gview?embedded=true&url=https://web.bprunisritama.com/upload/laporan/<?= $row->dokumen ?>" width='100%' height='420px' frameborder="0"></iframe>
                                 </div>
                                 <div class="modal-footer" style="margin-top: 25px;">
                                     <button type="button" class="btn btn-danger center-block" data-dismiss="modal" data-toggle="tooltip" data-placement="top" title="Tutup">Tutup</button>
