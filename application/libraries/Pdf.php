40d259cea87c0e7dc2ac2ebbb052087f718a9eea
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-07-23 17:18:55 +0700

    20200723-06

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-require_once dirname(__FILE__) . '\TCPDF\tcpdf.php';
+require_once dirname(__FILE__) . '\TCPDF-6.3.5\tcpdf.php';
 class Pdf extends TCPDF
 {
 
--- application/views/cetakData_nsb.php
+++ application/views/cetakData_nsb.php
@@ -2,14 +2,20 @@
 
 $pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
 $pdf->SetTitle('Pembukaan Rekening Tabungan Online - ' . $data_nsb->nm_lengkap);
-$pdf->setHeaderMargin(30);
-$pdf->SetTopMargin(20);
+$pdf->setHeaderMargin(5);
+// remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+// set font
+$pdf->SetFont('times', 'B', 10);
+
+$pdf->SetTopMargin(5);
 $pdf->setFooterMargin(20);
 $pdf->SetAutoPageBreak(true);
 $pdf->SetAuthor('BPR Unisritama');
 $pdf->SetDisplayMode('real', 'default');
 $pdf->AddPage();
-$html = '<h3>FORMULIR PEMBUKAAN TABUNGAN - ' . $data_nsb->nm_lengkap . '</h3>
+$html = '<h3 align="center">FORMULIR PEMBUKAAN REKENING TABUNGAN - ' . $data_nsb->nm_lengkap . '</h3>
 <table cellspacing="2">
                             <thead>
                                 <tr>
