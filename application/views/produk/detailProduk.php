a1798be1bb713099922c60649612cf6b0774fa14
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-09-24 16:55:28 +0700

    20200924

--- application/views/produk/detailProduk.php
+++ application/views/produk/detailProduk.php
@@ -4,9 +4,10 @@
         <div class="row">
             <div class="blog-details">
                 <!-- Start single blog -->
-                <div class="col-md-8 col-sm-8 col-xs-12">
+                <div class="col-md col-sm col-xs-12">
                     <!-- single-blog start -->
-                    <article class="blog-post-wrapper">
+                    <article class="blog-post-wrapper" style="margin-top: 35px;">
+                        <h3 class="text-center"><?= ucwords($row->produk); ?></h3>
                         <div class="blog-banner">
                             <a class="blog-images">
                                 <img src="<?= base_url('upload/produk/' . $row->image); ?>" alt="">
@@ -15,7 +16,7 @@
                                 <div class="blog-category" style="padding-top: 20px;">
                                     <!-- <h6><span class="label label-success"><?= $row->jenis; ?></span></h6> -->
                                 </div>
-                                <h3><?= strtoupper($row->produk); ?></h3>
+                                <!-- <h3><?= strtoupper($row->produk); ?></h3> -->
                                 <?php echo $row->deskripsi; ?>
                                 <a href="<?= base_url($row->link) ?>" class="btn btn-info btn btn-outline-success">Saya Berminat</a>
                             </div>
@@ -24,7 +25,7 @@
                     <div class="clear"></div>
                 </div>
             </div>
-            <?php $this->load->view('template/sidebar_produk'); ?>
+            <!-- <?//php $this->load->view('template/sidebar_produk'); ?> -->
         </div>
     </div>
 </div>
--- application/views/produk/pmb.php
+++ application/views/produk/pmb.php
@@ -4,19 +4,20 @@
         <div class="row">
             <div class="blog-details">
                 <!-- Start single blog -->
-                <div class="col-md-8 col-sm-8 col-xs-12">
+                <div class="col-md col-sm col-xs-12">
                     <!-- single-blog start -->
-                    <article class="blog-post-wrapper">
-                        <div class="blog-banner">
-                            <a href="#" class="blog-images">
-                                <img src="<?= base_url('upload/produk/' . $row->image); ?>" alt="">
-                            </a>
-                            <div class="blog-content bor text-justify" >
+                    <article class="blog-post-wrapper" style="margin-top: 35px;">
+                        <h3 class="text-center"><?= ucwords($row->produk); ?></h3>
+                        <div class="blog-banner" style="margin-top: 20px;">
+                            <!-- <a href="#" class="blog-images"> -->
+                            <img width="1333px" height="889px" src="<?= base_url('upload/produk/' . $row->image); ?>" class="blog-images" alt="">
+                            <!-- </a> -->
+                            <div class="blog-content bor text-justify">
                                 <div class="blog-category" style="padding-top: 20px;">
                                     <!-- <span><?= $row->jenis; ?></span> -->
                                     <h6><span class="label label-success"><?= $row->jenis; ?></span></h6>
                                 </div>
-                                <h3><?= strtoupper($row->produk); ?></h3>
+                                <!-- <h3><?= strtoupper($row->produk); ?></h3> -->
                                 <?= ucfirst($row->deskripsi); ?>
                             </div>
                         </div>
@@ -24,7 +25,7 @@
                     <div class="clear"></div>
                 </div>
             </div>
-            <?php $this->load->view('template/sidebar'); ?>
+            <!-- <?//php $this->load->view('template/sidebar'); ?> -->
         </div>
     </div>
 </div>
